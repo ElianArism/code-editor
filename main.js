0150b43df63d6f09dc383778cfba0d61260a68d8
ElianArism <elianarism@gmail.com>
2023-09-26 22:58:26 -0300

    set up proyect and implement hot reload for dev mode, also create initial structure and start implementing title bar

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 const { app, BrowserWindow } = require("electron");
 
+const electronReload = require("electron-reload");
+
+electronReload(__dirname);
+
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({ width: 800, height: 600 });
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minHeight: 800,
+    minWidth: 700,
+    frame: false,
+  });
+
   win.loadFile("index.html");
+
   win.webContents.openDevTools();
 
   win.on("closed", () => {
